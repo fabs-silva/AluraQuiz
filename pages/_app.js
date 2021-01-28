9749fb17e554a2e8040942dc5629458cb68c0685
Fabiana Silva <45769060+fabs-silva@users.noreply.github.com>
2021-01-28 10:47:10 -0300

    Adicionadas todas as metatags

--- pages/_app.js
+++ pages/_app.js
@@ -39,7 +39,22 @@ export default function App({ Component, pageProps }) {
         <title>ESCQuiz</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap" rel="stylesheet" />
+
+        <title>ESCQuiz - Quanto você sabe sobre Eurovision?</title>
+        <meta name="title" content="ESCQuiz - Quanto você sabe sobre Eurovision?" />
+        <meta name="description" content="Quiz em português sobre o Eurovision Song Contest, maior competição musical do mundo." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://escquiz.fabs-silva.vercel.app/" />
+        <meta property="og:title" content="ESCQuiz - Quanto você sabe sobre Eurovision?" />
+        <meta property="og:description" content="Quiz em português sobre o Eurovision Song Contest, maior competição musical do mundo." />
         <meta property="og:image" content="https://escquiz.fabs-silva.vercel.app/images/background.jpeg" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://escquiz.fabs-silva.vercel.app/" />
+        <meta property="twitter:title" content="ESCQuiz - Quanto você sabe sobre Eurovision?" />
+        <meta property="twitter:description" content="Quiz em português sobre o Eurovision Song Contest, maior competição musical do mundo." />
+        <meta property="twitter:image" content="https://escquiz.fabs-silva.vercel.app/images/background.jpeg" />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
